a1e44839d8edb30ffc9dc554280292aeda661127
Šimon Smejkal <simon.smejkal@seznam.cz>
2021-04-10 14:55:43 +0200

    change app url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const httpProxy = require("http-proxy");
 
 httpProxy
   .createProxyServer({
-    target: "http://40.127.216.33:3001",
+    target: "http://20.54.124.163:3001",
     ws: true
   })
   .listen(3331);
